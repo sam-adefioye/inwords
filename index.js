663e3e5cef236abc2d4615a43cb3534fd28e83e2
sam-adefioye <sam.adefioye@gmail.com>
2017-12-27 02:55:29 +0000

    Change

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + 'public'));
-
 app.set('views', __dirname + 'views');
 app.set('views engine', 'ejs');
 
