5e9ec7f133df5f321e61da78de736e6a9b288ef5
sam-adefioye <sam.adefioye@gmail.com>
2017-12-27 04:20:11 +0000

    Update

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const pg = require('pg')
 const express = require('express')
+const app = express()
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(__dirname + 'public'))
-  .set('views', __dirname + 'views')
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-// app.get('/login', (req, res) => res.render());
-// app.get('/new', (req, res) => res.render());
-// app.get('/help', (req, res) => res.render());
-// app.get('/about', (req, res) => res.render());
+app.use(express.static(__dirname + 'public'));
+app.set('views', __dirname + 'views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/login', (req, res) => res.render());
+app.get('/new', (req, res) => res.render());
+app.get('/help', (req, res) => res.render());
+app.get('/about', (req, res) => res.render());
 
 // app.route('/login')
 //   .get(function (req, res) {
 //
 //   })
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
