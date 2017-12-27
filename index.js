7e85bfe49e18edb634637b06dc6df22a0c4ca20a
sam-adefioye <sam.adefioye@gmail.com>
2017-12-27 04:38:59 +0000

    U

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var pg = require('pg');
-var express = require('express');
-var path = require('path');
-var app = express();
-var port = process.env.PORT || 5000;
+const pg = require('pg')
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
@@ -19,4 +19,4 @@ app.get('/about', (req, res) => res.render());
 //
 //   })
 
-app.listen(port);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
